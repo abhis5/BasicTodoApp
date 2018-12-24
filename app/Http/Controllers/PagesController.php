ae5ab2db4d5b73281e9138e0a6bd970e76c73bab
abhis5 <abhi02blp@gmail.com>
2018-12-24 11:44:27 +0530

    first commit

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+     
+     public function new1(){
+      
+      echo "some eco data";
+       # return view('new');
+
+     }
+
+
+
+
+}
---
+++ app/Http/Controllers/TodosController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Todo;
+
+class TodosController extends Controller
+{
+    public function index(){
+
+    $todos = Todo::all();
+
+    return view('todos') -> with('todos' , $todos);
+
+
+    }
+}
